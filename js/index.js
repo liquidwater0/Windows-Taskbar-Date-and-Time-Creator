1e27b54ec814ba4000f3f2173ee0da3c6501edfd
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2021-10-25 23:39:49 -0500

    custom colors functionality

--- js/index.js
+++ js/index.js
@@ -1,16 +1,37 @@
 const taskbarDateTime = document.getElementById("taskbarDateTime");
-const radioInputs = document.querySelectorAll("input[type='radio']");
+const inputs = document.querySelectorAll("input");
 
 const darkRadio = document.getElementById("darkMode");
+const lightRadio = document.getElementById("lightMode");
+const customRadio = document.getElementById("custom");
+
+const customColorsDiv = document.getElementById("customColors");
+const colorInputs = document.querySelectorAll("input[type='color']");
+const textColorInput = document.getElementById("textColorInput");
+const backgroundColorInput = document.getElementById("backColorInput");
+
 const win10Radio = document.getElementById("win10");
+
 const normalSizeRadio = document.getElementById("smallSize");
 
-radioInputs.forEach(function(radioInput) {
-    radioInput.addEventListener("input", update);
+inputs.forEach(function(input) {
+    input.addEventListener("input", update);
 });
 
 function update() {
-    darkRadio.checked ? taskbarDateTime.setAttribute("data-theme", "dark") : taskbarDateTime.setAttribute("data-theme", "light");
+    if (darkRadio.checked) {
+        taskbarDateTime.setAttribute("data-theme", "dark");
+    } else if (lightRadio.checked) {
+        taskbarDateTime.setAttribute("data-theme", "light");
+    } else {
+        taskbarDateTime.setAttribute("data-theme", "custom");
+        customColorsDiv.style.display = "block";
+        document.documentElement.style.setProperty("--customTextColor", textColorInput.value);
+        document.documentElement.style.setProperty("--customBackColor", backgroundColorInput.value);
+    }
+
+    if (!customRadio.checked) customColorsDiv.style.display = "none";
+
     win10Radio.checked ? taskbarDateTime.setAttribute("data-os", "win10") : taskbarDateTime.setAttribute("data-os", "win11");
     normalSizeRadio.checked ? taskbarDateTime.setAttribute("data-size", "normal") : taskbarDateTime.setAttribute("data-size", "big");
 }
