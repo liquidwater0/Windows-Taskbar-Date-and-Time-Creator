1ac2bded1e519d574b6e84e3feae8fe03aa73d99
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-08-18 16:10:22 -0500

    set initial date and time to local time in 12h

--- src/components/DateAndTime.jsx
+++ src/components/DateAndTime.jsx
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 
 export default function DateAndTime({ theme, os, size }) {
-    const [time, setTime] = useState("12:00 AM");
-    const [date, setDate] = useState("1/1/2000");
+    const [time, setTime] = useState(() => {
+        const now = new Date();
+        const hour = now.getHours() % 12;
+        const hourFormatted = hour === 0 ? 12 : hour;
+        const minute = now.getMinutes().toString().padStart(2, "0");
+        const amPm = now.getHours() >= 12 ? "PM" : "AM";
+
+        return `${hourFormatted}:${minute} ${amPm}`;
+    });
+    const [date, setDate] = useState(() => {
+        const now = new Date();
+        const month = now.getMonth() + 1;
+        const day = now.getDate();
+        const year = now.getFullYear();
+
+        return `${month}/${day}/${year}`;
+    });
 
     return (
         <div className='date-and-time' data-theme={theme} data-os={os} data-size={size}>
